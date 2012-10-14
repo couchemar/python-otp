1bd950f0de796d524c16bdf20a64ce1fd9ade921
Andrey Pavlov <couchemar@yandex.ru>
2012-10-14 16:15:15 +0400

    stub for node connection

---
+++ src/node/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import logging
+from gevent import sleep, socket, Greenlet
+
+
+class OutgoingNodeConnection(Greenlet):
+    logger = logging.getLogger('otp.node_connection')
+
+    def __init__(self, port, cookie):
+        pass
