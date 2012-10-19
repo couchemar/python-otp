40287ab465191a1473327e7c12d87c121089013b
Andrey Pavlov <couchemar@yandex.ru>
2012-10-19 17:52:00 +0400

    Start protocol for node handshake

--- src/node/__init__.py
+++ src/node/__init__.py
@@ -7,4 +7,17 @@ class OutgoingNodeConnection(Greenlet):
     logger = logging.getLogger('otp.node.connection')
 
     def __init__(self, port, cookie):
+        self.port= port
+        self.cookie = cookie
+
+    def connect(self):
+        try:
+            self.socket = socket.create_conenction(('localhost',
+                                                    self.port))
+            self._connected = True
+        except socket.error as exc:
+            self.logger.error('Could not connect to %s', ('localhost',
+                                                          self.port))
+
+    def send_name(self):
         pass
--- src/node/protocol.py
+++ src/node/protocol.py
@@ -1,2 +1,19 @@
 # coding: utf-8
+import struct
+from epmd.codecs import encode_request
+
+ DISTR_FLAG_PUBLISHED = 1
+ DISTR_FLAG_ATOMCACHE = 2
+ DISTR_FLAG_EXTENDEDREFERENCES = 4
+ DISTR_FLAG_DISTMONITOR = 8
+ DISTR_FLAG_FUNTAGS = 16
+ DISTR_FLAG_DISTMONITORNAME = 32
+ DISTR_FLAG_HIDDENATOMCACHE = 64
+ DISTR_FLAG_NEWFUNTAGS = 128
+ DISTR_FLAG_EXTENDEDPIDSPORTS = 256
+
+
+def encode_name(version, flags, node_name):
+    v_f = struct.pack('!HI', version, flags)
+    return "n{}{}".format(v_f, node_name)
 
