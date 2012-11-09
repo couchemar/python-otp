5c36208469ee87e90fedd1b51b4268258a154174
Andrey Pavlov <couchemar@yandex.ru>
2012-11-09 13:48:34 +0400

    small refactoring

--- src/node/__init__.py
+++ src/node/__init__.py
@@ -53,13 +53,12 @@ class OutgoingNodeConnection(Greenlet):
         self.logger.info('Receiving challenge')
         challenge = decode_challenge(self.socket)
         self.logger.info('Challenge received: %s', challenge)
-        self.out_challenge = challenge[3]
         return challenge
 
-    def send_challenge_reply(self):
+    def send_challenge_reply(self, out_challenge):
         self.logger.info('Sending challenge reply')
         self.challenge = gen_challenge()
-        digest = gen_digest(self.out_challenge, self.cookie)
+        digest = gen_digest(out_challenge, self.cookie)
         self._send(encode_challenge_reply(self.challenge, digest))
         self.logger.info('Challenge reply sended')
 
@@ -67,12 +66,31 @@ class OutgoingNodeConnection(Greenlet):
         self.logger.info('Receiving challenge ack')
         challenge_ack = decode_challenge_ack(self.socket)
         self.logger.info('Challenge ack received: %s', challenge_ack)
+        return challenge_ack
 
-        out_digest = challenge_ack[1]
-        if out_digest == gen_digest(self.challenge, self.cookie):
+    def do_handshake(self):
+        self.send_name()
+        (_, status) = self.recv_status()
+        if status == 'ok':
+            self.logger.info('Status is "ok"')
+        else:
+            self.logger.warning('Status is "%s" does not know '
+                                'what to do with it', status)
+            return
+        challenge = self.recv_challenge()
+        out_challenge = challenge[3]
+        self.send_challenge_reply(out_challenge)
+        challenge_ack = self.recv_challenge_ack()
+
+        if challenge_ack[1] == gen_digest(self.challenge, self.cookie):
             self.logger.info('Connection is up')
             self.state = 'connected'
-            return challenge_ack
         else:
             self.logger.warning('Cannot set up connection, '
                                 'because of digest missmatch.')
+
+
+class Node(Greenlet):
+    def __init__(self, node_name, port, cookie):
+        super(Node, self).__init__()
+
--- src/tests/node_tests.py
+++ src/tests/node_tests.py
@@ -6,28 +6,31 @@ from epmd import EPMDKeepAliveConnection, port2_please
 from node import OutgoingNodeConnection
 
 
-class NodeTestCase(unittest.TestCase):
+class OutgoingNodeConnectionTestCase(unittest.TestCase):
 
     def setUp(self):
         self.erl_node_name = 'erl1'
 
-    def test(self):
+    def test_handshake(self):
         conn = EPMDKeepAliveConnection('test', 9999)
         conn.start()
-        res = port2_please(self.erl_node_name)
 
+        res = port2_please(self.erl_node_name)
         erl_port = res[1]
 
         out_conn = OutgoingNodeConnection('test', erl_port, 'secret')
         out_conn.connect()
         out_conn.send_name()
         self.assertEqual(out_conn.recv_status(), ('s', 'ok'))
+
         res = out_conn.recv_challenge()
         self.assertEqual(len(res), 5)
         self.assertEqual(res[0], 'n')
+
         expected_node_name = self.erl_node_name + '@' + socket.gethostname()
         self.assertEqual(res[4], expected_node_name)
-        out_conn.send_challenge_reply()
+
+        out_conn.send_challenge_reply(res[3])
         res = out_conn.recv_challenge_ack()
         self.assertEqual(len(res), 2)
         self.assertEqual(res[0], 'a')
