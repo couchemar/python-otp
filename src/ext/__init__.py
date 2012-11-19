70d058a6b7b5455c60377aadd0c2483e01541f59
Andrey Pavlov <couchemar@yandex.ru>
2012-11-19 18:22:27 +0400

    Decode small_integer_ext ext

--- src/ext/__init__.py
+++ src/ext/__init__.py
@@ -2,11 +2,19 @@
 import struct
 
 
+def decode_small_integer_ext(data):
+    return struct.unpack('!B', data[0]), data[1:]
+
 def decode_small_tuple_ext(data):
     arity = struct.unpack('!B', data[0])
-    elements_data = data[1:]
-    decode_ext(elements_data)
-
+    rest = data[1:]
+    e = 0
+    _t = []
+    while e < arity:
+        el, rest = decode_ext(rest)
+        _t.append(el)
+        e += 1
+    return tuple(_t), rest
 
 def decode(data):
     [ext_type] = struct.unpack('!B', data[0])
@@ -16,5 +24,9 @@ def decode_ext(data):
     [ext_type] = struct.unpack('!B', data[0])
     return DECODERS[ext_type](data[1:])
 
+
+SMALL_INTEGER_EXT = 97
 SMALL_TUPLE_EXT = 104
-DECODERS = {SMALL_TUPLE_EXT: decode_small_tuple_ext}
+
+DECODERS = {SMALL_INTEGER_EXT: decode_small_integer_ext,
+            SMALL_TUPLE_EXT: decode_small_tuple_ext}
