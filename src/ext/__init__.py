2576307cb54664f55b2068c58f924d01187c1ab4
Andrey Pavlov <couchemar@yandex.ru>
2012-11-22 17:43:41 +0400

    Decode term

--- src/ext/__init__.py
+++ src/ext/__init__.py
@@ -42,12 +42,21 @@ def decode_small_tuple_ext(data):
     return tuple(_t), rest
 
 def decode(data):
-    [ext_type] = struct.unpack('!B', data[0])
-    return decode_ext(data[1:])
+    rest = data
+    _res = []
+    while rest:
+        msg, rest = decode_term(rest)
+        _res.append(msg)
+    return tuple(_res)
 
 def decode_ext(data):
     ext_type, rest = _decode_byte(data)
     return DECODERS[ext_type](rest)
+
+def decode_term(data):
+    version, rest = _decode_byte(data)
+    assert version == 131, "Unsuported version {}".format(version)
+    return decode_ext(rest)
 
 
 DECODERS = {SMALL_INTEGER_EXT: decode_small_integer_ext,
